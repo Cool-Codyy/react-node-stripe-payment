957e34178063e03b35a55fba6133dab26b3d32c3
balaji <balaji841990@gmail.com>
2023-04-27 00:02:15 +0530

    user register

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import PaymentScreen from "./PaymentScreen";
 import AddPayMethod from "./AddPayMethod";
-// import Register from "./Register";
+import Register from "./Register";
 import StripeWrapper from "./StripeWrapper";
 import PaymentSuccess from "./PaymentSuccess";
 import PaymentFailure from "./PaymentFailure";
@@ -14,11 +14,11 @@ function App() {
     <StripeWrapper>
       <BrowserRouter>
         <Switch>
-          {/* <Route path="/add-payment-method" component={AddPayMethod} /> */}
           <Route path="/make-payment" component={PaymentScreen} />
           <Route path="/payment-success" component={PaymentSuccess} />
           <Route path="/payment-failure" component={PaymentFailure} />
           <Route path="/all-transactions" component={AllTransactions} />
+          <Route path="/register" component={Register} />
           <Route path="/" component={AddPayMethod} />
         </Switch>
       </BrowserRouter>
