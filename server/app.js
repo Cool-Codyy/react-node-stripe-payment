cc0a7f820811fd43fda864c7de4e595b3a9f2b84
balaji <balaji841990@gmail.com>
2023-04-26 23:49:44 +0530

    testing11

--- server/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-app.use("/", (req, res) => {
-  res.json({ message: 'server is running..' });
-});
-
-const PORT = 5051;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
